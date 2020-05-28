576dd04fbe1d5c0f5259e62927c374aa9f40a1d0
Stenli <stanil_dimitrov@mail.bg>
2020-05-28 16:46:53 +0300

    Add Initial and Seed migrations.

---
+++ SampleApp/SampleApp.Core/Migrations/20200528134303_Seed.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleApp.Core.Migrations
+{
+    public partial class Seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Clients",
+                columns: new[] { "ClientId", "Age", "Gender", "Name" },
+                values: new object[,]
+                {
+                    { 1, 20, 0, "TestClient1" },
+                    { 2, 30, null, "TestClient2" },
+                    { 3, 35, 1, "TestClient3" },
+                    { 4, 40, 0, "TestClient4" },
+                    { 5, 50, null, "TestClient5" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "AccountId", "ClientId", "Sum", "Type" },
+                values: new object[,]
+                {
+                    { 1, 1, 1500m, 0 },
+                    { 2, 1, 2500m, 1 },
+                    { 3, 1, 2500m, 1 },
+                    { 4, 2, 3500m, 0 },
+                    { 5, 2, 2500m, 0 },
+                    { 6, 3, 1500m, 1 },
+                    { 7, 4, 2000m, 1 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Clients",
+                keyColumn: "ClientId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Clients",
+                keyColumn: "ClientId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Clients",
+                keyColumn: "ClientId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Clients",
+                keyColumn: "ClientId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Clients",
+                keyColumn: "ClientId",
+                keyValue: 4);
+        }
+    }
+}
